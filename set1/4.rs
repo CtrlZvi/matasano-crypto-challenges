b282a202c0bf61153b3fd48d3aa97c3247566123
Zvi "Viz" Effron <zeffron@riotgames.com>
2015-08-04 14:24:35 -0700

    Challenge 4 complete

--- set1/4.rs
+++ set1/4.rs
@@ -1,3 +1,7 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn hex2nibble(hex : u8) -> Result<u8, ()> {
     match hex {
         0x30u8 ... 0x39u8 => Ok(hex - 0x30u8),
@@ -17,48 +21,28 @@ fn hex2octets(hex: &Vec<u8>) -> Vec<u8> {
     octets
 }
 
-fn nibble2hex(nibble : u8) -> Result<u8, ()> {
-    match nibble {
-        0u8 ... 9u8 => Ok(nibble + 0x30u8),
-        10u8 ... 15u8 => Ok(nibble + 0x57u8),
-        _ => Err(()),
-    }
-}
-
-fn octets2hex(octets: Vec<u8>) -> Vec<u8> {
-    let mut hex : Vec<u8> = Vec::with_capacity(octets.len() * 2);
-    for octet in octets {
-        hex.push(nibble2hex((octet & 0xF0u8) >> 4).unwrap());
-        hex.push(nibble2hex(octet & 0x0Fu8).unwrap());
-    }
-    hex
-}
-
 fn fixed_xor(left: &Vec<u8>, right: &Vec<u8>) -> Vec<u8> {
     assert!(left.len() == right.len());
-    let left_octets = hex2octets(left);
-    let right_octets = hex2octets(right);
-    let mut xor : Vec<u8> = Vec::with_capacity(left_octets.len());
+    let mut xor : Vec<u8> = Vec::with_capacity(left.len());
     for i in 0 .. xor.capacity() {
-        xor.push(left_octets[i] ^ right_octets[i])
+        xor.push(left[i] ^ right[i])
     }
     xor
 }
 
-fn main() {
+fn decrypt_xor(input : &Vec<u8>) -> (u8, f32) {
     let letter_frequencies = vec![0.08167f32, 0.01492f32, 0.02782f32, 0.04253f32, 0.12702f32, 0.02228f32, 0.02015f32, 0.06094f32, 0.06966f32, 0.00153f32, 0.00772f32, 0.04025f32, 0.02406f32, 0.06749f32, 0.07507f32, 0.01929f32, 0.00095f32, 0.05987f32, 0.06327f32, 0.09056f32, 0.02758f32, 0.00978f32, 0.02361f32, 0.00150f32, 0.01974f32, 0.00074f32];
     assert!(letter_frequencies.len() == 26);
 
     let mut xor_character = 0u8;
     let mut xor_delta = std::f32::INFINITY;
 
-    let input = String::from("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").into_bytes();
     for c in 0u8 .. 127u8 {
         let mut pattern : Vec<u8> =  Vec::with_capacity(input.len());
-        for _ in 0 .. input.len() / 2 {
+        for _ in 0 .. input.len() {
             pattern.push(c)
         }
-        let output = fixed_xor(&input, &octets2hex(pattern));
+        let output = fixed_xor(&input, &pattern);
 
         let mut letter_count : Vec<f32> = Vec::with_capacity(letter_frequencies.len());
         for _ in 0 .. letter_frequencies.len() {
@@ -85,13 +69,39 @@ fn main() {
     }
 
     let mut pattern : Vec<u8> =  Vec::with_capacity(input.len());
-    for _ in 0 .. input.len() / 2 {
+    for _ in 0 .. input.len() {
         pattern.push(xor_character)
     }
-    let output = fixed_xor(&input, &octets2hex(pattern));
-    println!("Character: {}", xor_character);
-    println!("Delta: {}", xor_delta);
-    println!("String: {}", String::from_utf8(output).unwrap());
+
+    (xor_character, xor_delta)
+}
+
+fn main() {
+    let f = File::open("4.txt").unwrap();
+    let b = BufReader::new(&f);
+    let mut best_line = String::from("");
+    let mut key = 0u8;
+    let mut best_delta = std::f32::INFINITY;
+    for line in b.lines() {
+        let octets = line.unwrap().into_bytes();
+        let (c, delta) = decrypt_xor(&hex2octets(&octets));
+        if delta < best_delta {
+            best_delta = delta;
+            key = c;
+            best_line = String::from_utf8(octets).unwrap();
+        }
+    }
+
+    println!("Line: {}", best_line);
+    println!("Key: {}", key);
+    println!("Delta: {}", best_delta);
+
+    let input = hex2octets(&best_line.into_bytes());
+    let mut pattern : Vec<u8> =  Vec::with_capacity(input.len());
+    for _ in 0 .. input.len() {
+        pattern.push(key)
+    }
+    println!("Decrypted: {}", String::from_utf8(fixed_xor(&input, &pattern)).unwrap());
 }
 
 #[cfg(test)]
